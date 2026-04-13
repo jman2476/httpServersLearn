0e4b86adb7d128814824c4d674b0cb33def71259
Squirblechem <152367545+jman2476@users.noreply.github.com>
2026-04-13 15:50:13 -0400

    ch1 l11 custom handlers

--- main.go
+++ main.go
@@ -11,7 +11,9 @@ func main() {
 
 	serveMux := http.NewServeMux() //server multiplexer
 
-	serveMux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	rmPrefixHandler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
+	serveMux.Handle("/app/", rmPrefixHandler)
+	serveMux.HandleFunc("/healthz", handlerHealthz)
 
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -20,4 +22,11 @@ func main() {
 
 	log.Printf("Start serving from %s on port %s", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func handlerHealthz(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	// w.Header().Set("charset", "utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
