d9e517b4dde9f2935ec9c27b61ba70fec50a94a9
Jeremy McKeegan <152367545+jman2476@users.noreply.github.com>
2026-04-14 13:11:03 -0400

    ch2 l3 routing [missed commit on l2]

--- main.go
+++ main.go
@@ -1,22 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 func main() {
 	const port = "8080"
 	const filepathRoot = "."
+	var apiCfg apiConfig
+	mux := http.NewServeMux()
 
-	serveMux := http.NewServeMux() //server multiplexer
 	rmPrefixHandler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
-	serveMux.Handle("/app/", rmPrefixHandler)
-	serveMux.HandleFunc("/healthz", handlerHealthz)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(rmPrefixHandler))
+	mux.HandleFunc("GET /healthz", handlerHealthz)
+	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /reset", apiCfg.handleMetricReset)
 
 	server := &http.Server{
 		Addr:    ":" + port,
-		Handler: serveMux,
+		Handler: mux,
 	}
 
 	log.Printf("Start serving from %s on port %s", filepathRoot, port)
@@ -27,4 +32,32 @@ func handlerHealthz(w http.ResponseWriter, req *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
+	log.Println("Server status being checked")
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (c *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		c.fileserverHits.Add(1)
+		next.ServeHTTP(w, req)
+	})
+}
+
+func (c *apiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	hits := fmt.Sprint("Hits: ", c.fileserverHits.Load())
+	w.Write([]byte(hits))
+	log.Println("Metrics checked: ", c.fileserverHits.Load())
+}
+
+func (c *apiConfig) handleMetricReset(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	c.fileserverHits.Store(0)
+	w.Write([]byte("Server metrics reset"))
+	log.Println("Metrics reset")
 }
