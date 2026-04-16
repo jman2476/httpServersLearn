040e259796c535f9f1e85a8f367ec5ce16bc7626
Jeremy McKeegan <152367545+jman2476@users.noreply.github.com>
2026-04-16 12:51:38 -0400

    add mapUser function

--- users.go
+++ users.go
@@ -8,17 +8,19 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/jman2476/httpServersLearn/internal/database"
 )
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
+}
 
 func (cfg *apiConfig) handlerNewUser(w http.ResponseWriter, req *http.Request) {
 	type parameters struct {
 		Email string `json:"email"`
-	}
-	type returnVals struct {
-		ID         uuid.UUID `json:"id"`
-		Created_At time.Time `json:"created_at"`
-		Updated_At time.Time `json:"updated_at"`
-		Email      string    `json:"email"`
 	}
 
 	decoder := json.NewDecoder(req.Body)
@@ -45,5 +47,14 @@ func (cfg *apiConfig) handlerNewUser(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	respondWithJSON(w, 201, user)
+	respondWithJSON(w, 201, mapUser(user))
+}
+
+func mapUser(u database.User) User {
+	return User{
+		ID:        u.ID,
+		CreatedAt: u.CreatedAt,
+		UpdatedAt: u.UpdatedAt,
+		Email:     u.Email,
+	}
 }
