05a03df8819370a90dc1bad71c22250b3a950525
Squirblechem <152367545+jman2476@users.noreply.github.com>
2026-04-17 13:27:26 -0400

    add login endpoint

--- login.go
+++ login.go
@@ -3,6 +3,9 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"net/mail"
+
+	"github.com/jman2476/httpServersLearn/internal/auth"
 )
 
 func (cfg *apiConfig) handlerLogin(w http.ResponseWriter, req *http.Request) {
@@ -11,7 +14,31 @@ func (cfg *apiConfig) handlerLogin(w http.ResponseWriter, req *http.Request) {
 		Password string `json:"password"`
 	}
 
-	decoder := json.Decoder(req.Body)
+	decoder := json.NewDecoder(req.Body)
 	params := parameters{}
 	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 500, "Error decoding request", err)
+		return
+	}
+
+	email, err := mail.ParseAddress(params.Email)
+	if err != nil {
+		respondWithError(w, 400, "Invalid email address", err)
+		return
+	}
+
+	user, err := cfg.dbQueries.GetUserByEmail(req.Context(), email.Address)
+	if err != nil {
+		respondWithError(w, 401, "Forbidden", err)
+		return
+	}
+
+	ok, err := auth.CheckPasswordHash(params.Password, user.HashedPassword)
+	if !ok || err != nil {
+		respondWithError(w, 401, "Forbidden", err)
+		return
+	}
+
+	respondWithJSON(w, 200, mapUser(user))
 }
